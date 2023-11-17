585b3ab5237adf96e59994b456f4f7d492741f40
Edinho <git@final04.com>
2023-11-17 18:47:22 -0300

    add get_code function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-
+import datetime as dt
 import os
 from pathlib import Path
+
+import pyotp
+import pyperclip
 
 
 def _read_lines(filename: str = '.env') -> list[str]:
@@ -14,9 +17,35 @@ def _read_lines(filename: str = '.env') -> list[str]:
     return lines
 
 
-def _load(line_reader_func: callable = _read_lines) -> dict:
+def _load(_line_reader_func: callable = _read_lines) -> dict:
     """Loads 2fa codes from file to a dict"""
-    lines = line_reader_func()
+    lines = _line_reader_func()
     lines = [i.split('=') for i in lines if i != '']
     d = {i[0]: i[1] for i in lines}
     return d
+
+
+def get_code(
+    name: str,
+    _load_func: callable = _load,
+):
+    """Print the OTP code to screen and put it into memory."""
+
+    tokens = _load_func()
+
+    if name.endswith('_raw'):
+        code = tokens[name]
+        print(f'{code=}')
+    else:
+        name = name.lower()
+        key = tokens[name]
+        t = pyotp.TOTP(key)
+        code = t.now()
+
+        time = dt.datetime.now().second
+        time = time % 30
+        time = 30 - time
+        print(f'{code=} for {time} seconds')
+
+    pyperclip.copy(code)
+    return code
--- test_main.py
+++ test_main.py
@@ -1,3 +1,6 @@
+import pyotp
+import pyperclip
+
 import main
 
 
@@ -5,7 +8,12 @@ def mock_read_lines():
     return [
         'a=66666666666666666666666666666666',
         'b=CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC',
+        'c_raw=abc',
     ]
+
+
+def mock_load():
+    return main._load(mock_read_lines)
 
 
 def test_read_lines_return_list_with_correct_content():
@@ -29,4 +37,26 @@ def test_load_return_dict_with_code_a():
 
 def test_load_return_dict_with_len_2():
     r = main._load(mock_read_lines)
-    assert len(r) == 2
+    assert len(r) == 3
+
+
+def test_get_code_returns_string():
+    given = main.get_code('a', mock_load)
+    assert type(given) == str
+
+
+def test_get_code_returns_expected_result_for_raw():
+    given = main.get_code('c_raw', mock_load)
+    assert given == 'abc'
+
+
+def test_get_code_returns_expected_result_for_a():
+    given = main.get_code('a', mock_load)
+    expected = pyotp.TOTP('66666666666666666666666666666666').now()
+    assert given == expected
+
+
+def test_get_code_put_result_in_clipboard_for_a():
+    expected = main.get_code('a', mock_load)
+    given = pyperclip.paste()
+    assert given == expected
